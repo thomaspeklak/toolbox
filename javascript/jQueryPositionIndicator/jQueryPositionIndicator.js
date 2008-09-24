f91d8da68d8a942244ad5770c49860fc2ea9e23d
Thomas Peklak <thomas.peklak@gmail.com>
2008-09-24 14:12:56 +0200

    used the scope of positionIndicator to shorten functions

--- javascript/jQueryPositionIndicator/jQueryPositionIndicator.js
+++ javascript/jQueryPositionIndicator/jQueryPositionIndicator.js
@@ -2,24 +2,24 @@
 	$.browser.msieLt7=($.browser.msie && $.browser.version<7)?1:0;
 	$.positionIndicator = {
 		storeHeadingPositions : function(){
-			$.positionIndicator.storedPositions = [];
+			this.storedPositions = [];
 			$('h1,h2').each(function(){
 				$.positionIndicator.storedPositions.push([this.offsetTop,$(this).text()]);
 			});
-			$.positionIndicator.storedPositions.reverse();
+			this.storedPositions.reverse();
 		},
 		currentHeading : function(){
 			var fromTop=($.browser.msieLt7)?document.body.scrollTop-50:document.documentElement.scrollTop;
-			for(var i in $.positionIndicator.storedPositions){
-				if($.positionIndicator.storedPositions[i][0]<fromTop+80) return $.positionIndicator.storedPositions[i][1];
+			for(var i in this.storedPositions){
+				if(this.storedPositions[i][0]<fromTop+80) return this.storedPositions[i][1];
 			}
-			return $.positionIndicator.storedPositions[i][1];
+			return this.storedPositions[i][1];
 		},
 		setIEDisplayProperties : function(){
 			$('#positionIndicator').css('right',document.body.offsetWidth-document.body.clientWidth);
 		},
 		showCurrent : function(){
-			$('#positionIndicator').html($.positionIndicator.currentHeading()).fadeIn();
+			$('#positionIndicator').html(this.currentHeading()).fadeIn();
 		}
 	}
 	$(function(){
